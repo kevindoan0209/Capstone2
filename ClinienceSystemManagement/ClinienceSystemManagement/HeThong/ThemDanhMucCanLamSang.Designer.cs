8783d6c93807e36784150462181704472fa72863
Doan Cong Truc <kevindoan.0209@gmail.com>
2017-03-22 16:00:17 +0700

    Continue desgin UI of Paraclinical form

--- ClinienceSystemManagement/ClinienceSystemManagement/HeThong/ThemDanhMucCanLamSang.Designer.cs
+++ ClinienceSystemManagement/ClinienceSystemManagement/HeThong/ThemDanhMucCanLamSang.Designer.cs
@@ -30,42 +30,73 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThemDanhMucCanLamSang));
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.btnHuy = new DevExpress.XtraEditors.SimpleButton();
+            this.btnTao = new DevExpress.XtraEditors.SimpleButton();
             this.xtraTabContarol1 = new DevExpress.XtraTab.XtraTabControl();
             this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+            this.txtDanhMuc = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl8 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.txtDonVi = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.txtThamChieu = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.cbxPhanNhom = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.txtMa = new DevExpress.XtraEditors.TextEdit();
+            this.txtTen = new DevExpress.XtraEditors.TextEdit();
+            this.txtGiaTriNho = new DevExpress.XtraEditors.SpinEdit();
+            this.txtGiaTriLon = new DevExpress.XtraEditors.SpinEdit();
             this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
-            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.txtGhiChu = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl9 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl12 = new DevExpress.XtraEditors.LabelControl();
+            this.txtDiaChi = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl11 = new DevExpress.XtraEditors.LabelControl();
+            this.txtMoTa = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl10 = new DevExpress.XtraEditors.LabelControl();
+            this.txtTenKhac = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabContarol1)).BeginInit();
             this.xtraTabContarol1.SuspendLayout();
             this.xtraTabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDanhMuc.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDonVi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtThamChieu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbxPhanNhom.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMa.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTen.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiaTriNho.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiaTriLon.Properties)).BeginInit();
+            this.xtraTabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGhiChu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDiaChi.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMoTa.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTenKhac.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // panelControl1
             // 
-            this.panelControl1.Controls.Add(this.simpleButton2);
-            this.panelControl1.Controls.Add(this.simpleButton1);
+            this.panelControl1.Controls.Add(this.btnHuy);
+            this.panelControl1.Controls.Add(this.btnTao);
             resources.ApplyResources(this.panelControl1, "panelControl1");
             this.panelControl1.Name = "panelControl1";
             // 
-            // simpleButton1
+            // btnHuy
             // 
-            this.simpleButton1.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
-            resources.ApplyResources(this.simpleButton1, "simpleButton1");
-            this.simpleButton1.Name = "simpleButton1";
+            this.btnHuy.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            resources.ApplyResources(this.btnHuy, "btnHuy");
+            this.btnHuy.Name = "btnHuy";
             // 
-            // simpleButton2
+            // btnTao
             // 
-            this.simpleButton2.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
-            resources.ApplyResources(this.simpleButton2, "simpleButton2");
-            this.simpleButton2.Name = "simpleButton2";
+            this.btnTao.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            resources.ApplyResources(this.btnTao, "btnTao");
+            this.btnTao.Name = "btnTao";
             // 
             // xtraTabContarol1
             // 
@@ -78,37 +109,183 @@
             // 
             // xtraTabPage1
             // 
-            this.xtraTabPage1.Controls.Add(this.textEdit2);
-            this.xtraTabPage1.Controls.Add(this.textEdit1);
+            this.xtraTabPage1.Controls.Add(this.txtDanhMuc);
+            this.xtraTabPage1.Controls.Add(this.labelControl8);
+            this.xtraTabPage1.Controls.Add(this.labelControl7);
+            this.xtraTabPage1.Controls.Add(this.labelControl6);
+            this.xtraTabPage1.Controls.Add(this.txtDonVi);
+            this.xtraTabPage1.Controls.Add(this.labelControl5);
+            this.xtraTabPage1.Controls.Add(this.labelControl4);
+            this.xtraTabPage1.Controls.Add(this.txtThamChieu);
+            this.xtraTabPage1.Controls.Add(this.labelControl3);
+            this.xtraTabPage1.Controls.Add(this.cbxPhanNhom);
+            this.xtraTabPage1.Controls.Add(this.labelControl2);
+            this.xtraTabPage1.Controls.Add(this.labelControl1);
+            this.xtraTabPage1.Controls.Add(this.txtMa);
+            this.xtraTabPage1.Controls.Add(this.txtTen);
+            this.xtraTabPage1.Controls.Add(this.txtGiaTriNho);
+            this.xtraTabPage1.Controls.Add(this.txtGiaTriLon);
             this.xtraTabPage1.Name = "xtraTabPage1";
             resources.ApplyResources(this.xtraTabPage1, "xtraTabPage1");
             // 
+            // txtDanhMuc
+            // 
+            resources.ApplyResources(this.txtDanhMuc, "txtDanhMuc");
+            this.txtDanhMuc.Name = "txtDanhMuc";
+            // 
+            // labelControl8
+            // 
+            resources.ApplyResources(this.labelControl8, "labelControl8");
+            this.labelControl8.Name = "labelControl8";
+            // 
+            // labelControl7
+            // 
+            resources.ApplyResources(this.labelControl7, "labelControl7");
+            this.labelControl7.Name = "labelControl7";
+            // 
+            // labelControl6
+            // 
+            resources.ApplyResources(this.labelControl6, "labelControl6");
+            this.labelControl6.Name = "labelControl6";
+            // 
+            // txtDonVi
+            // 
+            resources.ApplyResources(this.txtDonVi, "txtDonVi");
+            this.txtDonVi.Name = "txtDonVi";
+            // 
+            // labelControl5
+            // 
+            resources.ApplyResources(this.labelControl5, "labelControl5");
+            this.labelControl5.Name = "labelControl5";
+            // 
+            // labelControl4
+            // 
+            resources.ApplyResources(this.labelControl4, "labelControl4");
+            this.labelControl4.Name = "labelControl4";
+            // 
+            // txtThamChieu
+            // 
+            resources.ApplyResources(this.txtThamChieu, "txtThamChieu");
+            this.txtThamChieu.Name = "txtThamChieu";
+            // 
+            // labelControl3
+            // 
+            resources.ApplyResources(this.labelControl3, "labelControl3");
+            this.labelControl3.Name = "labelControl3";
+            // 
+            // cbxPhanNhom
+            // 
+            resources.ApplyResources(this.cbxPhanNhom, "cbxPhanNhom");
+            this.cbxPhanNhom.Name = "cbxPhanNhom";
+            this.cbxPhanNhom.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(((DevExpress.XtraEditors.Controls.ButtonPredefines)(resources.GetObject("comboBoxEdit1.Properties.Buttons"))))});
+            // 
+            // labelControl2
+            // 
+            resources.ApplyResources(this.labelControl2, "labelControl2");
+            this.labelControl2.Name = "labelControl2";
+            // 
+            // labelControl1
+            // 
+            resources.ApplyResources(this.labelControl1, "labelControl1");
+            this.labelControl1.Name = "labelControl1";
+            // 
+            // txtMa
+            // 
+            resources.ApplyResources(this.txtMa, "txtMa");
+            this.txtMa.Name = "txtMa";
+            // 
+            // txtTen
+            // 
+            resources.ApplyResources(this.txtTen, "txtTen");
+            this.txtTen.Name = "txtTen";
+            // 
+            // txtGiaTriNho
+            // 
+            resources.ApplyResources(this.txtGiaTriNho, "txtGiaTriNho");
+            this.txtGiaTriNho.Name = "txtGiaTriNho";
+            this.txtGiaTriNho.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(((DevExpress.XtraEditors.Controls.ButtonPredefines)(resources.GetObject("textEdit5.Properties.Buttons"))))});
+            this.txtGiaTriNho.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Default;
+            this.txtGiaTriNho.Properties.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            131072});
+            this.txtGiaTriNho.Properties.Mask.MaskType = ((DevExpress.XtraEditors.Mask.MaskType)(resources.GetObject("textEdit5.Properties.Mask.MaskType")));
+            // 
+            // txtGiaTriLon
+            // 
+            resources.ApplyResources(this.txtGiaTriLon, "txtGiaTriLon");
+            this.txtGiaTriLon.Name = "txtGiaTriLon";
+            this.txtGiaTriLon.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(((DevExpress.XtraEditors.Controls.ButtonPredefines)(resources.GetObject("textEdit6.Properties.Buttons"))))});
+            this.txtGiaTriLon.Properties.EditValueChangedFiringMode = DevExpress.XtraEditors.Controls.EditValueChangedFiringMode.Default;
+            this.txtGiaTriLon.Properties.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            131072});
+            this.txtGiaTriLon.Properties.Mask.MaskType = ((DevExpress.XtraEditors.Mask.MaskType)(resources.GetObject("textEdit6.Properties.Mask.MaskType")));
+            // 
             // xtraTabPage2
             // 
+            this.xtraTabPage2.Controls.Add(this.txtGhiChu);
+            this.xtraTabPage2.Controls.Add(this.labelControl9);
+            this.xtraTabPage2.Controls.Add(this.labelControl12);
+            this.xtraTabPage2.Controls.Add(this.txtDiaChi);
+            this.xtraTabPage2.Controls.Add(this.labelControl11);
+            this.xtraTabPage2.Controls.Add(this.txtMoTa);
+            this.xtraTabPage2.Controls.Add(this.labelControl10);
+            this.xtraTabPage2.Controls.Add(this.txtTenKhac);
             this.xtraTabPage2.Name = "xtraTabPage2";
             resources.ApplyResources(this.xtraTabPage2, "xtraTabPage2");
             // 
-            // textEdit1
+            // txtGhiChu
             // 
-            resources.ApplyResources(this.textEdit1, "textEdit1");
-            this.textEdit1.Name = "textEdit1";
+            resources.ApplyResources(this.txtGhiChu, "txtGhiChu");
+            this.txtGhiChu.Name = "txtGhiChu";
             // 
-            // textEdit2
+            // labelControl9
             // 
-            resources.ApplyResources(this.textEdit2, "textEdit2");
-            this.textEdit2.Name = "textEdit2";
+            resources.ApplyResources(this.labelControl9, "labelControl9");
+            this.labelControl9.Name = "labelControl9";
             // 
-            // panelControl2
+            // labelControl12
             // 
-            resources.ApplyResources(this.panelControl2, "panelControl2");
-            this.panelControl2.Name = "panelControl2";
+            resources.ApplyResources(this.labelControl12, "labelControl12");
+            this.labelControl12.Name = "labelControl12";
+            // 
+            // txtDiaChi
+            // 
+            resources.ApplyResources(this.txtDiaChi, "txtDiaChi");
+            this.txtDiaChi.Name = "txtDiaChi";
+            // 
+            // labelControl11
+            // 
+            resources.ApplyResources(this.labelControl11, "labelControl11");
+            this.labelControl11.Name = "labelControl11";
+            // 
+            // txtMoTa
+            // 
+            resources.ApplyResources(this.txtMoTa, "txtMoTa");
+            this.txtMoTa.Name = "txtMoTa";
+            // 
+            // labelControl10
+            // 
+            resources.ApplyResources(this.labelControl10, "labelControl10");
+            this.labelControl10.Name = "labelControl10";
+            // 
+            // txtTenKhac
+            // 
+            resources.ApplyResources(this.txtTenKhac, "txtTenKhac");
+            this.txtTenKhac.Name = "txtTenKhac";
             // 
             // ThemDanhMucCanLamSang
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.xtraTabContarol1);
-            this.Controls.Add(this.panelControl2);
             this.Controls.Add(this.panelControl1);
             this.Name = "ThemDanhMucCanLamSang";
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
@@ -116,9 +293,21 @@
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabContarol1)).EndInit();
             this.xtraTabContarol1.ResumeLayout(false);
             this.xtraTabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.xtraTabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDanhMuc.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDonVi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtThamChieu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbxPhanNhom.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMa.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTen.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiaTriNho.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGiaTriLon.Properties)).EndInit();
+            this.xtraTabPage2.ResumeLayout(false);
+            this.xtraTabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGhiChu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDiaChi.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMoTa.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTenKhac.Properties)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -126,13 +315,34 @@
         #endregion
 
         private DevExpress.XtraEditors.PanelControl panelControl1;
-        private DevExpress.XtraEditors.SimpleButton simpleButton2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton btnHuy;
+        private DevExpress.XtraEditors.SimpleButton btnTao;
         private DevExpress.XtraTab.XtraTabControl xtraTabContarol1;
         private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+        private DevExpress.XtraEditors.TextEdit txtMa;
+        private DevExpress.XtraEditors.TextEdit txtTen;
         private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
-        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.TextEdit txtDonVi;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.MemoEdit txtThamChieu;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.ComboBoxEdit cbxPhanNhom;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.MemoEdit txtDanhMuc;
+        private DevExpress.XtraEditors.LabelControl labelControl8;
+        private DevExpress.XtraEditors.SpinEdit txtGiaTriNho;
+        private DevExpress.XtraEditors.SpinEdit txtGiaTriLon;
+        private DevExpress.XtraEditors.MemoEdit txtGhiChu;
+        private DevExpress.XtraEditors.LabelControl labelControl9;
+        private DevExpress.XtraEditors.LabelControl labelControl12;
+        private DevExpress.XtraEditors.TextEdit txtDiaChi;
+        private DevExpress.XtraEditors.LabelControl labelControl11;
+        private DevExpress.XtraEditors.MemoEdit txtMoTa;
+        private DevExpress.XtraEditors.LabelControl labelControl10;
+        private DevExpress.XtraEditors.TextEdit txtTenKhac;
     }
 }
--- ClinienceSystemManagement/ClinienceSystemManagement/HeThong/ThemDanhMucCanLamSang.cs
+++ ClinienceSystemManagement/ClinienceSystemManagement/HeThong/ThemDanhMucCanLamSang.cs
@@ -17,5 +17,7 @@ namespace ClinienceSystemManagement.HeThong
         {
             InitializeComponent();
         }
+
+       
     }
 }
