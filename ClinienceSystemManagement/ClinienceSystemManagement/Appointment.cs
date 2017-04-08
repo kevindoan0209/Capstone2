cfc5e9fd8e595e304dd2287b3e04f3ae286bb859
Doan Cong Truc <kevindoan.0209@gmail.com>
2017-04-08 11:39:41 +0700

    Continue design UI of Sub Appointment

---
+++ ClinienceSystemManagement/ClinienceSystemManagement/Appointment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraScheduler;
+
+namespace ClinienceSystemManagement.DatHen
+{
+    public partial class Appointment : DevExpress.XtraEditors.XtraForm
+    {
+        public Appointment()
+        {
+            InitializeComponent();
+        }
+
+        private void Appointment_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'clinienceSystemManagementDataSet.Resources' table. You can move, or remove it, as needed.
+            this.resourcesTableAdapter.Fill(this.clinienceSystemManagementDataSet.Resources);
+            // TODO: This line of code loads data into the 'clinienceSystemManagementDataSet.Appointments' table. You can move, or remove it, as needed.
+            this.appointmentsTableAdapter.Fill(this.clinienceSystemManagementDataSet.Appointments);
+
+        }
+
+        private void schedulerStorage1_AppointmentsChanged(object sender, PersistentObjectsEventArgs e)
+        {
+            appointmentsTableAdapter.Update(clinienceSystemManagementDataSet);
+            clinienceSystemManagementDataSet.AcceptChanges();
+        }
+
+        private void schedulerControl1_EditAppointmentFormShowing(object sender, AppointmentFormEventArgs e)
+        {
+            DevExpress.XtraScheduler.SchedulerControl scheduler = ((DevExpress.XtraScheduler.SchedulerControl)(sender));
+            ClinienceSystemManagement.CustomAppointmentForm form = new ClinienceSystemManagement.CustomAppointmentForm(scheduler, e.Appointment, e.OpenRecurrenceForm);
+            try
+            {
+                e.DialogResult = form.ShowDialog();
+                e.Handled = true;
+            }
+            finally
+            {
+                form.Dispose();
+            }
+
+        }
+    }
+}
